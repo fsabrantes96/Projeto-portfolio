1b475ccaa69f975f65335849b58ccc8230ba5e33
fsabrantes <fsabrantes96@gmail.com>
2023-02-09 11:23:52 -0300

    Removendo diretorio desnecessario

--- ProjetoJobFinder/db/connection.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './db/app.db'
-});
-
-// para exportar a conexão
-module.exports = sequelize
